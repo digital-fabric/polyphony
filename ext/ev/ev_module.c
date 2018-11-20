e34029111e282a9a188406c96b0b5ab71c1a7d49
Sharon Rosner <ciconia@gmail.com>
2018-11-20 18:37:20 +0100

    Implement EV.snooze in C ext

--- ext/ev/ev_module.c
+++ ext/ev/ev_module.c
@@ -9,6 +9,7 @@ static VALUE EV_ref(VALUE self);
 static VALUE EV_unref(VALUE self);
 
 static VALUE EV_next_tick(VALUE self);
+static VALUE EV_snooze(VALUE self);
 
 static VALUE watcher_refs;
 static VALUE next_tick_procs;
@@ -28,9 +29,10 @@ void Init_EV() {
   rb_define_singleton_method(mEV, "ref", EV_ref, 0);
   rb_define_singleton_method(mEV, "unref", EV_unref, 0);
   rb_define_singleton_method(mEV, "next_tick", EV_next_tick, 0);
+  rb_define_singleton_method(mEV, "snooze", EV_snooze, 0);
 
-  ID_call = rb_intern("call");
-  ID_each = rb_intern("each");
+  ID_call   = rb_intern("call");
+  ID_each   = rb_intern("each");
 
   watcher_refs = rb_hash_new();
   rb_global_variable(&watcher_refs);
@@ -82,8 +84,35 @@ static VALUE EV_next_tick(VALUE self) {
   return Qnil;
 }
 
+static VALUE EV_snooze(VALUE self) {
+  VALUE ret;
+  VALUE fiber = rb_fiber_current();
+
+  rb_ary_push(next_tick_procs, fiber);
+  if (!next_tick_active) {
+    next_tick_active = 1;
+    ev_timer_start(EV_DEFAULT, &next_tick_timer);
+  }
+
+  ret = rb_fiber_yield(0, 0);
+  
+  // fiber is resumed, check if resumed value is an exception
+  if (RTEST(rb_obj_is_kind_of(ret, rb_eException))) {
+    rb_ary_delete(next_tick_procs, fiber);
+    return rb_funcall(ret, rb_intern("raise"), 1, ret);
+  }
+  else {
+    return ret;
+  }
+}
+
 VALUE EV_next_tick_caller(VALUE proc, VALUE data, int argc, VALUE* argv) {
-  rb_funcall(proc, ID_call, 1, Qtrue);
+  if (rb_obj_is_proc(proc)) {
+    rb_funcall(proc, ID_call, 1, Qtrue);
+  }
+  else {
+    rb_fiber_resume(proc, 0, 0);
+  }
   return Qnil;
 }
 
