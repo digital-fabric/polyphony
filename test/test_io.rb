fcfc1eebb39eba19c59d9ee2725c398b85638aa9
Sharon Rosner <ciconia@gmail.com>
2019-05-19 20:34:54 +0200

    More IO testing

--- test/test_io.rb
+++ test/test_io.rb
@@ -8,16 +8,16 @@ module IOTests
   class IOTest < MiniTest::Test
     def setup
       EV.rerun
+      @i, @o = IO.pipe
     end
 
-    def test_that_io_does_not_block
-      i, o = IO.pipe
+    def test_that_io_op_yields_to_other_fibers
       count = 0
       msg = nil
       [
         spawn {
-          o.write("hello")
-          o.close
+          @o.write("hello")
+          @o.close
         },
 
         spawn {
@@ -28,11 +28,21 @@ module IOTests
         }, 
 
         spawn {
-          msg = i.read
+          msg = @i.read
         }
       ].each(&:await)
       assert_equal(5, count)
       assert_equal("hello", msg)
     end
+
+    def test_that_double_chevron_method_returns_io
+      assert_equal(@o, @o << 'foo')
+
+      @o << 'bar' << 'baz'
+      @o.close
+      assert_equal('foobarbaz', @i.read)
+    end
+
+    def test_
   end
 end
