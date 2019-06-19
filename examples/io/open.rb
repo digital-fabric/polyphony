5b4f2f30693efd3c077ac3a58c81088df9e62320
Sharon Rosner <ciconia@gmail.com>
2019-06-19 17:07:42 +0200

    Add IO.open example

---
+++ examples/io/open.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'polyphony'
+require 'irb'
+
+stdin = IO.open(STDIN.to_i)
+
+loop do
+  print "Say something: "
+  cancel_after(3) do
+    line = stdin.gets
+    puts "You said: #{line}"
+  end
+rescue Polyphony::Cancel
+  puts "<got nothing>"
+end
