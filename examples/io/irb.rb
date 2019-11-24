dd434702bb5b5109ad777f464a5000a225d7fd48
Sharon Rosner <ciconia@gmail.com>
2019-11-24 18:33:17 +0100

    Fixed IRB example

--- examples/io/irb.rb
+++ examples/io/irb.rb
@@ -4,16 +4,24 @@ require 'bundler/setup'
 require 'polyphony/auto_run'
 require 'irb'
 
-# For some reason, this example does not work
-
 $counter = 0
 timer = spin do
-  throttled_loop(1) do
+  throttled_loop(5) do
     $counter += 1
-    p Time.now
+  end
+end
+
+# readline blocks the current thread, so we offload it to the blocking-ops
+# thread pool. That way, the reactor loop can keep running while waiting for
+# readline to return
+module ::Readline
+  alias_method :orig_readline, :readline
+  def readline(*args)
+    Polyphony::ThreadPool.process { orig_readline(*args) }
   end
 end
 
 at_exit { timer.stop }
 
+puts "try typing $counter to see the counter incremented in the background"
 IRB.start
