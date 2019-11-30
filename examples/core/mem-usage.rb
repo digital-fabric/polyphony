51492471461367daf15bfb7c3e927f161a68fd04
Sharon Rosner <ciconia@gmail.com>
2019-11-30 07:58:17 +0100

    Add memory usage example

---
+++ examples/core/mem-usage.rb
@@ -0,0 +1,34 @@
+require 'fiber'
+
+def mem_usage
+  `ps -o rss #{$$}`.strip.split.last.to_i
+end
+
+def calculate_fiber_memory_cost(count)
+  GC.disable
+  rss0 = mem_usage
+  count.times { Fiber.new { sleep 1 } }
+  rss1 = mem_usage
+  GC.start
+  cost = (rss1 - rss0).to_f / count
+
+  puts "fiber memory cost: #{cost}KB"
+end
+
+calculate_fiber_memory_cost(10000)
+
+require 'bundler/setup'
+require 'polyphony'
+
+def calculate_coprocess_memory_cost(count)
+  GC.disable
+  rss0 = mem_usage
+  count.times { spin { :foo } }
+  rss1 = mem_usage
+  GC.start
+  cost = (rss1 - rss0).to_f / count
+
+  puts "coprocess memory cost: #{cost}KB"
+end
+
+calculate_coprocess_memory_cost(10000)
