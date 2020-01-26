dae6a5c0292d585dc08af40f91aa06db2b7cf758
Sharon Rosner <ciconia@gmail.com>
2020-01-26 06:26:18 +0100

    Correctly increment ref count for indefinite sleep

--- lib/polyphony/core/global_api.rb
+++ lib/polyphony/core/global_api.rb
@@ -66,10 +66,17 @@ module API
   end
 
   def sleep(duration = nil)
-    return suspend unless duration
+    return sleep_forever unless duration
 
     timer = Gyro::Timer.new(duration, 0)
     timer.await
+  end
+
+  def sleep_forever
+    Thread.current.fiber_ref
+    suspend
+  ensure
+    Thread.current.fiber_unref
   end
 
   def supervise(&block)
