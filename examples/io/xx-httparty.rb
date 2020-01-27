1fea920532e653202e39b86b51373c28d4b7a60c
Sharon Rosner <ciconia@gmail.com>
2020-01-27 11:48:25 +0100

    Update HTTParty example

--- examples/io/xx-httparty.rb
+++ examples/io/xx-httparty.rb
@@ -3,11 +3,36 @@
 require 'bundler/setup'
 require 'polyphony'
 require 'httparty'
+require 'json'
 
-timer = spin { throttled_loop(100) { 
-  STDOUT << '.'
-} }
+def get_time(tzone)
+  res = HTTParty.get("http://worldtimeapi.org/api/timezone/#{tzone}")
+  return '- failed -' unless res.ok?
 
-res = HTTParty.get('http://worldtimeapi.org/api/timezone/Europe/Paris')
-puts res
-timer.stop
+  json = JSON.parse(res.body)
+  Time.parse(json['datetime'])
+end
+
+zones = %w{
+  Europe/London Europe/Paris Europe/Bucharest America/New_York Asia/Bangkok
+}
+# zones.each do |tzone|
+#   spin do
+#     time = get_time(tzone)
+#     puts "Time in #{tzone}: #{time}"
+#   end
+# end
+
+# suspend
+
+def get_times(zones)
+  Polyphony::Supervisor.new do |s|
+    zones.each do |tzone|
+      s.spin { [tzone, get_time(tzone)] }
+    end
+  end
+end
+
+get_times(zones).await.each do |tzone, time|
+  puts "Time in #{tzone}: #{time}"
+end
