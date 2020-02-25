f6fe48aae9522c2ac153ef188e58c614b95b1c6f
Sharon Rosner <ciconia@gmail.com>
2020-02-25 07:35:51 +0100

    Work on fixing IRB adapter in Ruby 2.7 (#5)

--- lib/polyphony/adapters/irb.rb
+++ lib/polyphony/adapters/irb.rb
@@ -2,18 +2,45 @@
 
 require 'polyphony'
 
-# readline blocks the current thread, so we offload it to the blocking-ops
-# thread pool. That way, the reactor loop can keep running while waiting for
-# readline to return
-module ::Readline
-  alias_method :orig_readline, :readline
-
-  def readline(*args)
-    async = Gyro::Async.new
-    Thread.new do
-      result = orig_readline(*args)
-      async.signal!(result)
+if Object.constants.include?(:Reline)
+  class Reline::ANSI
+    def self.select(read_ios = [], write_ios = [], error_ios = [], timeout = nil)
+      p [:select, read_ios]
+      raise if read_ios.size > 1
+      raise if write_ios.size > 0
+      raise if error_ios.size > 0
+      
+      fiber = Fiber.current
+      timer = spin do
+        sleep timeout
+        fiber.cancel!
+      end
+      read_ios.each do |io|
+        io.read_watcher.await
+        return [io]
+      end
+    rescue Polyphony::Cancel
+      return nil
+    ensure
+      timer.stop
     end
-    async.await
   end
 end
+  
+  # readline blocks the current thread, so we offload it to the blocking-ops
+  # thread pool. That way, the reactor loop can keep running while waiting for
+  # readline to return
+  module ::Readline
+    alias_method :orig_readline, :readline
+
+    def readline(*args)
+      p :readline
+      async = Gyro::Async.new
+      Thread.new do
+        result = orig_readline(*args)
+        async.signal!(result)
+      end
+      async.await
+    end
+  end
+# end
