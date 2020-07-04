46ee5d04d62639f9e053d617fcad231c75551f9f
Sharon Rosner <ciconia@gmail.com>
2020-07-05 01:03:09 +0200

    Fix openssl extension

--- lib/polyphony/extensions/openssl.rb
+++ lib/polyphony/extensions/openssl.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'openssl'
-
 require_relative './socket'
 
 # Open ssl socket helper methods (to make it compatible with Socket API)
@@ -18,12 +17,36 @@ class ::OpenSSL::SSL::SSLSocket
     io.reuse_addr
   end
 
-  def sysread(maxlen, buf)
+  alias_method :orig_accept, :accept
+  def accept
+    loop do
+      result = accept_nonblock(exception: false)
+      case result
+      when :wait_readable then Thread.current.agent.wait_io(io, false)
+      when :wait_writable then Thread.current.agent.wait_io(io, true)
+      else
+        return result
+      end
+    end
+  end
+
+  alias_method :orig_sysread, :sysread
+  def sysread(maxlen, buf = +'')
     loop do
       case (result = read_nonblock(maxlen, buf, exception: false))
       when :wait_readable then Thread.current.agent.wait_io(io, false)
+      else return result
+      end
+    end
+  end
+
+  alias_method :orig_syswrite, :syswrite
+  def syswrite(buf)
+    loop do
+      case (result = write_nonblock(buf, exception: false))
       when :wait_writable then Thread.current.agent.wait_io(io, true)
-      else result
+      else
+        return result
       end
     end
   end
@@ -37,13 +60,14 @@ class ::OpenSSL::SSL::SSLSocket
     # @sync = osync
   end
 
-  def syswrite(buf)
-    loop do
-      case (result = write_nonblock(buf, exception: false))
-      when :wait_readable then Thread.current.agent.wait_io(io, false)
-      when :wait_writable then Thread.current.agent.wait_io(io, true)
-      else result
-      end
+  def readpartial(maxlen, buf = +'')
+    result = sysread(maxlen, buf)
+    result || (raise EOFError)
+  end
+
+  def read_loop
+    while (data = sysread(8192))
+      yield data
     end
   end
 end
