f55f5ac0fc827522c377759e1230eeebe10bb89f
Sharon Rosner <ciconia@gmail.com>
2020-07-09 08:55:16 +0200

    More accurate mem usage stats

--- examples/performance/mem-usage.rb
+++ examples/performance/mem-usage.rb
@@ -4,47 +4,53 @@ def mem_usage
   `ps -o rss #{$$}`.split.last.to_i
 end
 
-def calculate_fiber_memory_cost(count)
+def calculate_memory_cost(name, count, &block)
+  GC.enable
+  ObjectSpace.garbage_collect
+  sleep 0.5
   GC.disable
   rss0 = mem_usage
-  count.times { Fiber.new { sleep 1 } }
+  count0 = ObjectSpace.count_objects[:TOTAL] - ObjectSpace.count_objects[:FREE]
+  a = []
+  count.times { a << block.call }
   rss1 = mem_usage
-  GC.start
+  count1 = ObjectSpace.count_objects[:TOTAL] - ObjectSpace.count_objects[:FREE]
+  p [count0, count1]
+  # sleep 0.5
   cost = (rss1 - rss0).to_f / count
+  count_delta = (count1 - count0) / count
 
-  puts "fiber memory cost: #{cost}KB"
+  puts "#{name} rss cost: #{cost}KB     object count: #{count_delta}"
 end
 
-calculate_fiber_memory_cost(10000)
+f = Fiber.new { |f| f.transfer }
+f.transfer Fiber.current
 
-def calculate_thread_memory_cost(count)
-  GC.disable
-  rss0 = mem_usage
-  count.times { Thread.new { sleep 1 } }
-  sleep 0.5
-  rss1 = mem_usage
-  sleep 0.5
-  GC.start
-  cost = (rss1 - rss0).to_f / count
-
-  puts "thread memory cost: #{cost}KB"
+calculate_memory_cost('fiber', 10000) do
+  f = Fiber.new { |f| f.transfer :foo }
+  f.transfer Fiber.current
+  f
 end
 
-calculate_thread_memory_cost(500)
+t = Thread.new { sleep 1}
+t.kill
+t.join
+
+calculate_memory_cost('thread', 500) do
+  t = Thread.new { sleep 1 }
+  sleep 0.001
+  t
+end
+(Thread.list - [Thread.current]).each(&:kill).each(&:join)
 
 require 'bundler/setup'
 require 'polyphony'
 
-def calculate_extended_fiber_memory_cost(count)
-  GC.disable
-  rss0 = mem_usage
-  count.times { spin { :foo } }
-  snooze
-  rss1 = mem_usage
-  GC.start
-  cost = (rss1 - rss0).to_f / count
+f = spin { sleep 0.1 }
+f.await
 
-  puts "extended fiber memory cost: #{cost}KB"
+calculate_memory_cost('polyphony fiber', 10000) do
+  f = spin { :foo }
+  f.await
+  f
 end
-
-calculate_extended_fiber_memory_cost(10000)
