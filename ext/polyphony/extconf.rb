c6f48fee70cca22c21ce7eb95fb61c5875a4536b
Sharon Rosner <sharon@noteflakes.com>
2020-09-20 16:04:59 +0000

    Extract liburing code, successful compile

--- ext/polyphony/extconf.rb
+++ ext/polyphony/extconf.rb
@@ -9,44 +9,29 @@ force_use_libev = ENV['POLYPHONY_USE_LIBEV'] != nil
 if !force_use_libev && RUBY_PLATFORM =~ /linux/ && `uname -srm` =~ /Linux (5\.\d)/
   kernel_version = $1.gsub('.', '').to_i
   $defs << "-DPOLYPHONY_KERNEL_VERSION_#{kernel_version}"
-  case kernel_version
-  when 55..59
+  if kernel_version >= 54
     use_liburing = true
-    $defs << "-DPOLYPHONY_IO_URING"
+    $defs << "-DPOLYPHONY_IO_URING_TIMEOUT"
+  end
+  if kernel_version >= 55
     $defs << "-DPOLYPHONY_IO_URING_ACCEPT"
     $defs << "-DPOLYPHONY_IO_URING_CONNECT"
     $defs << "-DPOLYPHONY_IO_URING_ASYNC_CANCEL"
-  when 54
-    use_liburing = true
-    $defs << "-DPOLYPHONY_IO_URING"
-    $defs << "-DPOLYPHONY_IO_URING_TIMEOUT"
   end
 end
 
 if use_liburing
-  puts "Compiling liburing"
-  ext_dir = File.join(FileUtils.pwd, RbConfig::CONFIG["srcdir"], '..')
-  `cd #{ext_dir}/liburing && make`
-
-  lib_dir = RbConfig::CONFIG['libdir']
-  include_dir = RbConfig::CONFIG['includedir']
-
-  header_dirs = [include_dir, File.join(ext_dir, 'liburing/src/include')]
-  lib_dirs = [lib_dir, File.join(ext_dir, 'liburing/src')]
-  dir_config('polyphony', header_dirs, lib_dirs)
-
   $defs << "-DPOLYPHONY_BACKEND_LIBURING"
 else
   $defs << "-DPOLYPHONY_BACKEND_LIBEV"
+  $defs << '-DEV_USE_LINUXAIO'     if have_header('linux/aio_abi.h')
+  $defs << '-DEV_USE_SELECT'       if have_header('sys/select.h')
+  $defs << '-DEV_USE_POLL'         if have_type('port_event_t', 'poll.h')
+  $defs << '-DEV_USE_EPOLL'        if have_header('sys/epoll.h')
+  $defs << '-DEV_USE_KQUEUE'       if have_header('sys/event.h') && have_header('sys/queue.h')
+  $defs << '-DEV_USE_PORT'         if have_type('port_event_t', 'port.h')
+  $defs << '-DHAVE_SYS_RESOURCE_H' if have_header('sys/resource.h')  
 end
-
-$defs << '-DEV_USE_LINUXAIO'     if have_header('linux/aio_abi.h')
-$defs << '-DEV_USE_SELECT'       if have_header('sys/select.h')
-$defs << '-DEV_USE_POLL'         if have_type('port_event_t', 'poll.h')
-$defs << '-DEV_USE_EPOLL'        if have_header('sys/epoll.h')
-$defs << '-DEV_USE_KQUEUE'       if have_header('sys/event.h') && have_header('sys/queue.h')
-$defs << '-DEV_USE_PORT'         if have_type('port_event_t', 'port.h')
-$defs << '-DHAVE_SYS_RESOURCE_H' if have_header('sys/resource.h')
 
 CONFIG['optflags'] << ' -fno-strict-aliasing' unless RUBY_PLATFORM =~ /mswin/
 
--- ext/polyphony/libev.c
+++ ext/polyphony/libev.c
@@ -1,2 +1,6 @@
+#ifdef POLYPHONY_BACKEND_LIBEV
+
 #include "libev.h"
 #include "../libev/ev.c"
+
+#endif // POLYPHONY_BACKEND_LIBEV
