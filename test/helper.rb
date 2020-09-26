55ef6b5b88b17e69d35cd2b1a1fe66cec0173978
Sharon Rosner <sharon@noteflakes.com>
2020-09-26 13:19:38 +0000

    Debug problem with cancellation

--- test/helper.rb
+++ test/helper.rb
@@ -22,6 +22,20 @@ class ::Fiber
   attr_writer :auto_watcher
 end
 
+module ::Kernel
+  def trace(*args)
+    STDOUT.orig_write(format_trace(args))
+  end
+
+  def format_trace(args)
+    if args.size > 1 && args.first.is_a?(String)
+      format("%s: %p\n", args.shift, args.size == 1 ? args.first : args)
+    else
+      format("%p\n", args.size == 1 ? args.first : args)
+    end
+  end
+end
+
 class MiniTest::Test
   def setup
     # puts "* setup #{self.name}"
