81e0d9bda55a589b94dee1f33de4e89fc04975a2
Sharon Rosner <sharon@noteflakes.com>
2020-10-03 14:08:49 +0000

    Redesign op context
    - Allocate on heap using linked list storage for both available and taken contexts
    - Release a context using a two-stage process, in order to prevent a race condition upon completion / fiber resumption (whichever comes first)
    - Simplify all backend functions

---
+++ ext/polyphony/backend_io_uring_context.h
@@ -0,0 +1,46 @@
+#ifndef BACKEND_IO_URING_CONTEXT_H
+#define BACKEND_IO_URING_CONTEXT_H
+
+#include "ruby.h"
+
+enum op_type {
+  OP_NONE,
+  OP_READV,
+  OP_WRITEV,
+  OP_RECV,
+  OP_SEND,
+  OP_TIMEOUT,
+  OP_POLL,
+  OP_ACCEPT,
+  OP_CONNECT
+};
+
+typedef struct op_context {
+  struct op_context *next;
+  enum op_type      type: 16;
+  int               completed : 16;
+  int               result;
+  VALUE             fiber;
+} op_context_t;
+
+typedef struct op_context_store {
+  op_context_t *available;
+  op_context_t *taken;
+} op_context_store_t;
+
+const char *op_type_to_str(enum op_type type);
+
+void context_store_initialize(op_context_store_t *store);
+op_context_t *context_store_borrow(op_context_store_t *store, enum op_type type);
+void context_store_return(op_context_store_t *store, op_context_t *ctx);
+void context_store_free(op_context_store_t *store);
+
+#define OP_CONTEXT_ACQUIRE(store, op_type) context_store_borrow(store, op_type)
+#define OP_CONTEXT_RELEASE(store, ctx) { \
+  if (ctx->completed) \
+    context_store_return(store, ctx); \
+  else \
+    ctx->completed = 1; \
+}
+
+#endif /* BACKEND_IO_URING_CONTEXT_H */
