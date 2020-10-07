b5d0e5e252bb788efb5109376cce92a95fff8fa7
Sharon Rosner <sharon@noteflakes.com>
2020-10-07 12:27:21 +0000

    Fix upgrade-kernel script

--- bin/upgrade-kernel.sh
+++ bin/upgrade-kernel.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+
 echo Kernel version: $(uname -r)
 echo Updating...
-wget https://raw.githubusercontent.com/pimlie/ubuntu-mainline-kernel.sh/
+
+wget https://raw.githubusercontent.com/pimlie/ubuntu-mainline-kernel.sh/master/ubuntu-mainline-kernel.sh
 chmod +x ubuntu-mainline-kernel.sh
 ./ubuntu-mainline-kernel.sh --yes
+
 echo Kernel version: $(uname -r)
