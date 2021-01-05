3d0e602e3cbfffb372a21b6cd1b4b66aa4bb228e
Sharon Rosner <sharon@noteflakes.com>
2021-01-05 14:13:14 +0000

    Check line splitting performance

---
+++ examples/performance/line_splitting.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "benchmark/ips"
+
+def slice
+  str = ('*' * 40) + "\n" + ('*' * 40) + "\n" + ('*' * 40) + "\n" + ('*' * 40) + "\n" + ('*' * 40)
+  lines = []
+  while true
+    idx = str.index("\n")
+    break unless idx
+    
+    lines << str.slice!(0, idx + 1)
+  end
+  raise unless lines.size == 4
+  raise unless str == ('*' * 40)
+end
+
+def split
+  str = ('*' * 40) + "\n" + ('*' * 40) + "\n" + ('*' * 40) + "\n" + ('*' * 40) + "\n" + ('*' * 40)
+  lines = str.split("\n")
+  if str[-1] == "\n"
+    str = ''
+  else
+    str = lines.pop
+  end
+  raise unless lines.size == 4
+  raise unless str == ('*' * 40)
+end
+
+Benchmark.ips do |x|
+  x.report("slice") { slice }
+  x.report("split") { split }
+  x.compare!
+end
