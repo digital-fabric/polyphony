2d4bd6cbdfa78296059358dc19d6d4e1854c121c
Sharon Rosner <sharon@noteflakes.com>
2021-06-14 16:05:56 +0000

    Add HTTPS server example

---
+++ examples/io/https_server.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'polyphony'
+require 'localhost/authority'
+
+authority = Localhost::Authority.fetch
+opts = {
+  reuse_addr:     true,
+  dont_linger:    true,
+  secure_context: authority.server_context
+}
+
+server = Polyphony::Net.tcp_listen('localhost', 1234, opts)
+
+puts 'Serving HTTPS on port 1234'
+while (socket = server.accept)
+  spin do
+    while (data = socket.gets("\n", 8192))
+      if data.chomp.empty?
+        socket << "HTTP/1.1 200 OK\nConnection: close\nContent-Length: 4\n\nfoo\n"
+        break
+      end
+    end
+  end
+end
