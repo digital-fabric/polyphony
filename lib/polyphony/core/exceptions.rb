c2167b268e687c66050a6821870ca5cf3b242103
Sharon Rosner <sharon@noteflakes.com>
2022-02-19 15:56:09 +0100

    Add missing docs for Polyphony exceptions (#75)

--- lib/polyphony/core/exceptions.rb
+++ lib/polyphony/core/exceptions.rb
@@ -1,15 +1,22 @@
 # frozen_string_literal: true
 
 module Polyphony
-  # Common exception class for interrupting fibers. These exceptions allow
-  # control of fibers. BaseException exceptions can encapsulate a value and thus
-  # provide a way to interrupt long-running blocking operations while still
-  # passing a value back to the call site. BaseException exceptions can also
-  # references a cancel scope in order to allow correct bubbling of exceptions
-  # through nested cancel scopes.
+
+  # Base exception class for interrupting fibers. These exceptions allow control
+  # of fibers. BaseException exceptions can encapsulate a value and thus provide
+  # a way to interrupt long-running blocking operations while still passing a
+  # value back to the call site. BaseException exceptions can also references a
+  # cancel scope in order to allow correct bubbling of exceptions through nested
+  # cancel scopes.
   class BaseException < ::Exception
+
+    # Exception value, used mainly for `MoveOn` exceptions.
     attr_reader :value
 
+    # Initializes the exception, setting the caller and the value.
+    #
+    # @param value [any] Exception value
+    # @return [void]
     def initialize(value = nil)
       @caller_backtrace = caller
       @value = value
@@ -33,10 +40,18 @@ module Polyphony
 
   # Interjection is used to run arbitrary code on arbitrary fibers at any point
   class Interjection < BaseException
+    
+    # Initializes an Interjection with the given proc.
+    #
+    # @param proc [Proc] interjection proc
+    # @return [void]
     def initialize(proc)
       @proc = proc
     end
 
+    # Invokes the exception by calling the associated proc.
+    #
+    # @return [void]
     def invoke
       @proc.call
     end
