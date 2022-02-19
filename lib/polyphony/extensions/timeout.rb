3eebf66373de54b87d5c299c87111160cbaa7ac3
Sharon Rosner <sharon@noteflakes.com>
2022-02-19 21:21:24 +0100

    Add docs for Timeout module (#75)

--- lib/polyphony/extensions/timeout.rb
+++ lib/polyphony/extensions/timeout.rb
@@ -2,8 +2,19 @@
 
 require 'timeout'
 
-# Override Timeout to use cancel scope
+# Timeout extensions
 module ::Timeout
+
+  # Sets a timeout for the given block. This method provides an equivalent API
+  # to the stock Timeout API provided by Ruby. In case of a timeout, the block
+  # will be interrupted and an exception will be raised according to the given
+  # arguments.
+  #
+  # @param sec [Number] timeout period in seconds
+  # @param klass [Class] exception class
+  # @param message [String] exception message
+  # @param &block [Proc] code to run
+  # @return [any] block's return value
   def self.timeout(sec, klass = Timeout::Error, message = 'execution expired', &block)
     cancel_after(sec, with_exception: [klass, message], &block)
   end
