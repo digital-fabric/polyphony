a9059badc3fbe2cad36ba12ea7c93e38fe3734f3
Sharon Rosner <sharon@noteflakes.com>
2022-02-28 21:16:35 +0100

    Add sketch for zlib stream example

---
+++ examples/core/zlib_stream.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'polyphony'
+require 'zlib'
+
+r, w = IO.pipe
+writer = Zlib::GzipWriter.new(w)
+
+writer << 'chunk'
+writer.flush
+p pos: writer.pos
+w.close
+
+p r.read
